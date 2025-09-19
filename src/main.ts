f3fee14c0f61b86e7871ce8e64a59314cf83d015
ronalcc13 <ronaldocc13@gmail.com>
2025-09-18 20:58:01 -0400

    datis

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { App } from './app/app';
+import { provideServiceWorker } from '@angular/service-worker';
+import { isDevMode } from '@angular/core';
+
+bootstrapApplication(App, {
+  providers: [
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+    }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
+  ],
+}).catch((err) => console.error(err));
